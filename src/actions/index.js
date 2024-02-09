08598ae4f4b8b59a19755c5862113efa8b40c0f9
Mamed <deff2666@gmail.com>
2024-02-10 00:51:58 +0600

    Added Redux Thunk Functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const filterHeroes = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+        .then(data => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()))
+}
+
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
@@ -36,12 +50,13 @@ export const filtersFetchingError = () => {
     }
 }
 
-export const activeFilterChanged = (filter) => {
-    return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
-}
+export const activeFilterChanged = (filter) => (dispatch) => {
+    setTimeout(() => {
+       dispatch({
+       type: 'ACTIVE_FILTER_CHANGED',
+       payload: filter
+   })
+    }, 1000)
 
 export const heroCreated = (hero) => {
     return {
